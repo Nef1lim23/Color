24ab5e11e1f41458d3295d4a60e85c192c6997b3
Nef1lim23 <56215877+Nef1lim23@users.noreply.github.com>
2019-10-31 12:57:35 +0300

    Add files via upload

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -1,31 +1,88 @@
 package com.company;
 
-import java.util.Arrays;
+import java.util.Random;
 
-public class Main {
+public class Main
+{
+    public static void main(String[] args)
+    {
+        Color.RED.printColor();
+        Color.GREEN.printColor();
+        Color.BLUE.printColor();
+        Color.YELLOW.printColor();
+        Color.PURPLE.printColor();
+        Color.CYAN.printColor();
 
-    public static void main(String[] args) {
-        System.out.println(Arrays.toString(Color.values()));
+        System.out.println(" - простой текст");
+        System.out.println(" - " + Color.getRandomLetter());
     }
 
-    public enum Color {
-        Green("Зеленый"),
-        Blue("Синий"),
-        Red("Красный");
+    enum Color
+    {
+        RED("Красный"), BLUE("Синий"), GREEN("Зелёный"), YELLOW("Желтый"), PURPLE("пурпурный"),CYAN("циан");
 
-        private String title;
+        private String color;
+        public static final String ANSI_RESET = "\u001B[0m";
+        public static final String ANSI_RED = "\u001B[31m";
+        public static final String ANSI_GREEN = "\u001B[32m";
+        public static final String ANSI_BLUE = "\u001B[34m";
+        public static final String ANSI_YELLOW = "\u001B[33m";
+        public static final String ANSI_PURPLE = "\u001B[35m";
+        public static final String ANSI_CYAN = "\u001B[36m";
+        Color(String color)
+        {
+            this.color = color;
+        }
 
-        Color(String title){
-        this.title=title;
-        }
-        public String getTitle(){
-            return title;
+        //random block
+        private static final
+        Color[] VALUES = values();
+
+        private static final
+        int SIZE = VALUES.length;
+
+        private static final
+        Random RANDOM = new Random();
+        public static Color getRandomLetter() {
+            return VALUES[RANDOM.nextInt(SIZE)]; }
+
+
+        public void printColor()
+        {
+            switch (color) {
+                case "Красный": {
+                    System.out.println(ANSI_RED + color + ANSI_RESET);
+                    break;
+                }
+                case "Синий": {
+                    System.out.println(ANSI_BLUE + color + ANSI_RESET);
+                    break;
+                }
+                case "Зелёный": {
+                    System.out.println(ANSI_GREEN + color + ANSI_RESET);
+                    break;
+                }
+                case "Желтый": {
+                    System.out.println(ANSI_YELLOW + color + ANSI_RESET);
+                    break;
+                }
+                case "циан":  {
+                    System.out.println(ANSI_CYAN + color + ANSI_RESET);
+                    break;
+                }
+                case "пурпурный": {
+                    System.out.println(ANSI_PURPLE + color + ANSI_RESET);
+                    break;
+                }
+
+                default:
+                    System.out.println("error");
+            }
+
         }
 
-        @Override
-        public String toString(){
-            return "Цвет: " +
-                      title ;
-        }
     }
+
 }
+
+
